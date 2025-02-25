6240f4f7032d79ffe0162cd3abd3129d90a54627
rupashi bangla club <rupashi.bangla.club@gmail.com>
2025-02-26 05:53:49 +0600

    Auth Changes

--- src/Controller/UserAuth/UserAuthPostController.js
+++ src/Controller/UserAuth/UserAuthPostController.js
@@ -4,13 +4,23 @@ const { authAccessKey } = require("../../secret");
 const UserAuthPostController = async (req, res) => {
   try {
     const { email, name, role } = req.body;
-    const token = jwt.sign({ email }, authAccessKey, { expiresIn: "24h" });
     const isExist = await authUser.findOne({ email });
     if (isExist) {
+      const jwtTokenValue = { 
+      email:isExist?.email,
+      name:isExist?.name,
+      role:isExist?.role }
+      const token = jwt.sign(jwtTokenValue, authAccessKey, { expiresIn: "24h" });
       return res.status(200).send(token);
     }
     if (!isExist?.email) {
-      const authValue = await authUser.insertOne({ email, name, role });
+       await authUser.insertOne({ email, name, role });
+      const jwtTokenData = {
+        email,
+        name,
+        role
+      }
+      const token = jwt.sign( jwtTokenData, authAccessKey, { expiresIn: "24h" });
       return res.status(200).send(token);
     }
   } catch (error) {

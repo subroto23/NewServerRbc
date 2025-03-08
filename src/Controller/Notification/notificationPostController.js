1c7e1915a38b1477272af98d8263adfe8f52689b
rupashi bangla club <rupashi.bangla.club@gmail.com>
2025-03-09 04:53:20 +0600

    TOken Set

--- src/Controller/Notification/notificationPostController.js
+++ src/Controller/Notification/notificationPostController.js
@@ -2,7 +2,19 @@ const { notification } = require("../../Dbconfig/DatabaseConfig");
 
 const notificationPostController = async (req, res) => {
   try {
+    const token = req?.body?.token;
+    const email = req?.body?.email;
     const isExist = await notification.findOne({ email: req?.body?.email });
+    if (isExist) {
+      if (isExist.token !== token) {
+        const result = await notification.updateOne(
+          { email },
+          { $set: { token } }
+        );
+        return res.status(200).send(result);
+      }
+    }
+
     if (!isExist && req?.body?.token && req?.body?.email) {
       const result = await notification.insertOne(req?.body);
       res.status(200).send(result);
